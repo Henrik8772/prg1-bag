95382adc9cb71fce82151640eb90602a05edacd7
henrik8772 <henrik.johansson@elev.ga.ntig.se>
2025-11-04 13:42:14 +0100

    code 1

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 bag = True
 inventory = []
-print("Welcome to your inventory!")
+print("Welcome to your inventory!🎒\n")
 
 while bag:
 
     print("Show items [I]")
     print("Save in the bag [S]")
     print("End program [E]")
+    print("Remove from bag [R] \n")
     choice = input("Choose: ")
 
     if choice.lower() == "i":
-        for thing in inventory:
-            print(thing)
+        if inventory:
+            print("This is your inventory!😃")
+            for thing in inventory:
+                print(thing)
+        else:
+            print("Your bag is empty!😭")
 
     elif choice.lower() == "s":
-        inventory.append(input("Write what you want to save: "))
+        items = input(
+            "Write what you want to save (seperate them with commas): ")
+        new_items = [item.strip() for item in items.split(",")]
+        inventory.extend(new_items)
+
+    elif choice.lower() == "r":
+        print("These are all the items.")
+        for thing in inventory:
+            print(thing)
+        inventory.remove(input("Write what you want to remove: "))
 
     elif choice.lower() == "e":
         bag = False
